5be7d75d700d375bf916e96e3e44bb0af974f78a
mydungeon <jonnydungeon@gmail.com>
2023-10-29 09:56:51 -0400

    update env variables in manifest files for mongo

--- backend/config/custom-environment-variables.ts
+++ backend/config/custom-environment-variables.ts
@@ -1,6 +1,7 @@
 export default {
-  dbUser: "MONGODB_USERNAME",
-  dbPass: "MONGODB_PASSWORD",
+  dbName: "MONGO_INITDB_DATABASE",
+  dbUser: "MONGO_INITDB_ROOT_USERNAME",
+  dbPass: "MONGO_INITDB_ROOT_PASSWORD",
   accessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
   accessTokenPublicKey: "ACCESS_TOKEN_PUBLIC_KEY",
   refreshTokenPrivateKey: "REFRESH_TOKEN_PRIVATE_KEY",
