f5a44a402993c0d996792e51ed8473ebd8df8d1b
mydungeon <jonnydungeons@gmail.com>
2024-01-06 21:01:08 -0500

    remove unused reference

--- frontend/src/ui/features/Toolbars/Site/Site.tsx
+++ frontend/src/ui/features/Toolbars/Site/Site.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { faCircleHalfStroke } from "@fortawesome/free-solid-svg-icons";
-import SiteToolbarProps from "./Site.types";
 import Breadcrumbs from "src/ui/components/Breadcrumbs";
 import { toggleTheme } from "src/redux/features/ui.slice";
 import Icon from "src/ui/components/Icon";
