a43b7729b8124fc147d165e28ea27a550b0bf12e
mydungeon <jonnydungeons@gmail.com>
2024-01-07 00:10:25 -0500

    add ludwig animation, update unauthorized page

--- frontend/src/ui/pages/Unauthorized/Unauthorized.tsx
+++ frontend/src/ui/pages/Unauthorized/Unauthorized.tsx
@@ -3,5 +3,13 @@ import Page from "src/ui/components/Page/Page";
 import "./Unauthorized.styles.scss";
 
 export default function UnauthorizedPage() {
-  return <Page classNames="unauthorized" pageTitleText="Unauthorized" />;
+  return (
+    <Page classNames="unauthorized" pageTitleText="Unauthorized">
+      <img
+        alt="Ludwig says nope"
+        width="400"
+        src="/assets/animations/ludwig.gif"
+      />
+    </Page>
+  );
 }
