bdf66a050dc90893cb70f462975fd202c89801fc
mydungeon <jonnydungeons@gmail.com>
2024-01-08 00:43:30 -0500

    update not found page

--- frontend/src/ui/pages/NotFound/NotFound.tsx
+++ frontend/src/ui/pages/NotFound/NotFound.tsx
@@ -3,5 +3,13 @@ import Page from "src/ui/components/Page";
 import "./NotFound.styles.scss";
 
 export default function NotFoundPage() {
-  return <Page classNames="notFound" pageTitleText="Not Found" />;
+  return (
+    <Page classNames="notFound" pageTitleText="Not Found">
+      <img
+        alt="Ludwig says nope"
+        width="400"
+        src="/assets/animations/ludwig.gif"
+      />
+    </Page>
+  );
 }
