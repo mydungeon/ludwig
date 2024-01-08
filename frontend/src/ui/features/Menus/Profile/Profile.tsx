eab66eabbccb29b227b86e2ecc94eed486ac755b
mydungeon <jonnydungeons@gmail.com>
2024-01-08 00:41:09 -0500

    update profile menu links

--- frontend/src/ui/features/Menus/Profile/Profile.tsx
+++ frontend/src/ui/features/Menus/Profile/Profile.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Profile.styles.scss";
+import Icon from "src/ui/components/Icon";
+import {
+  faChartLine,
+  faCircleUser,
+  faShield,
+  faUsers,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function ProfileSideMenu() {
   return (
     <div className="menu profile" data-testid="profile">
-      <h2>My Profile</h2>
+      <h2>My Links</h2>
       <div className="sub">
-        <Link to="profile/edit">Edit Profile</Link>
-        <Link to="profile/role/edit">Edit Role</Link>
-        <Link to="users">Users</Link>
+        <div>
+          <Icon icon={faCircleUser} />
+          <Link to="profile/edit">Edit Profile</Link>
+        </div>
+        <div>
+          <Icon icon={faShield} />
+          <Link to="profile/role/edit">Edit Role</Link>
+        </div>
+        <div>
+          <Icon icon={faUsers} />
+          <Link to="users">Users</Link>
+        </div>
+        <div>
+          <Icon icon={faChartLine} />
+          <Link to="metrics">Metrics</Link>
+        </div>
       </div>
     </div>
   );
