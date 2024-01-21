b5d5bc9396b64fe930da97deea28defd4dcf544b
mydungeon <jonnydungeons@gmail.com>
2024-01-21 08:50:33 -0500

    update userpfp, add state

--- frontend/src/ui/components/UserPfp/UserPfp.tsx
+++ frontend/src/ui/components/UserPfp/UserPfp.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import UserPfpProps from "./UserPfp.types";
 import "./UserPfp.styles.scss";
 import { randomIntFromInterval } from "src/utils";
 
 export default function UserPfp({ children }: UserPfpProps) {
-  const randomInt = randomIntFromInterval(1, 5);
+  const [randomInt, setRandomInt] = useState(randomIntFromInterval(1, 5));
   return (
     <div className="userPfp" data-testid="userPfp">
       <img alt="randomPfp" src={`../../../assets/pfps/rando${randomInt}.svg`} />
