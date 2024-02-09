88f7da0481ca4c8f2591447694b6a69e2fd5e53e
mydungeon <jonnydungeons@gmail.com>
2024-02-08 21:02:34 -0500

    update filter input component

--- frontend/src/ui/components/Table/Table.constants.ts
+++ frontend/src/ui/components/Table/Table.constants.ts
@@ -3,10 +3,10 @@ export const PAGE_SIZE = 8;
 export const MAX_CELL_TEXT_LENGTH = 8;
 
 export const USER_TABLE_COLUMNS = [
-  { name: "_id", filter: true },
+  { name: "_id" },
   { name: "name", sort: true, type: "string", filter: true },
   { name: "email", sort: true, type: "string", filter: true },
-  { name: "roles", filter: true },
+  { name: "roles" },
   { name: "createdAt", sort: true, type: "number" },
   { name: "updatedAt", sort: true, type: "number" },
   { name: "lastLoggedIn", sort: true, type: "number" },
